84477d7f605e5aac097656b224db221fd798f702
myeonggu.kim <myeonggu.kim@kakao.com>
2023-08-01 11:55:07 +0900

    initial commit

--- bash-init.sh
+++ bash-init.sh
@@ -5,10 +5,10 @@ BASEDIR=$( cd $( dirname $0 ) && pwd -P )
 cat << EOF > ~/.bash_profile
 #!/bin/bash
 
-PATH="\$PATH:$BASEDIR"
-PATH="\$PATH:$BASEDIR/bin"
-PATH="\$PATH:$BASEDIR/bin/bcomp"
-PATH="\$PATH:$BASEDIR/backup"
+PATH="$BASEDIR:\$PATH"
+PATH="$BASEDIR/bin:\$PATH"
+PATH="$BASEDIR/bin/bcomp:\$PATH"
+PATH="$BASEDIR/backup:\$PATH"
 export PATH
 EOF
 
