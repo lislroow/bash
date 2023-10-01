6874ff1429502d282de9f83030ed1a9e9315d892
myeonggu.kim <myeonggu.kim@kakao.com>
2023-10-01 20:55:25 +0900

    init

--- init-bash.sh
+++ init-bash.sh
@@ -15,8 +15,8 @@ export PATH
 alias gobash="cd '$BASHDIR'"
 alias gospring="cd '/c/project/spring'"
 
-alias backup-s='backup.sh --drive=s itunes movie tv'
-alias backup-t='backup.sh --drive=t itunes develop project wallpaper editplus'
+alias backup-s='backup.sh --drive=s itunes'
+alias backup-v='backup.sh --drive=v itunes 유틸리티 develop project wallpaper editplus'
 alias backup-u='backup.sh --drive=u itunes'
 
 EOF
@@ -33,8 +33,8 @@ cat << EOF
   alias gobash="cd '$BASHDIR'"
   alias gospring="cd '/c/project/spring'"
   
-  alias backup-s='backup.sh --drive=s itunes movie tv'
-  alias backup-t='backup.sh --drive=t itunes develop project wallpaper editplus'
+  alias backup-s='backup.sh --drive=s itunes'
+  alias backup-v='backup.sh --drive=v itunes 유틸리티 develop project wallpaper editplus'
   alias backup-u='backup.sh --drive=u itunes'
 
 EOF
