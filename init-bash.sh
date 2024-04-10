e9eff29603bfa8eb453689ef1bf4b68d2a082247
김명구 <myeonggu.kim@kakao.com>
2024-04-10 13:55:09 +0900

    java mvn 추가

--- init-bash.sh
+++ init-bash.sh
@@ -5,6 +5,13 @@ BASHDIR=$( cd $( dirname $0 ) && pwd -P )
 cat << EOF > ~/.bash_profile
 #!/bin/bash
 
+set -o vi
+
+export JAVA_HOME="/c/develop/tools/java/openjdk-17.0.3.0.6-1"
+PATH="\$JAVA_HOME/bin:\$PATH"
+export MAVEN_HOME="/c/develop/tools/maven"
+PATH="\$MAVEN_HOME/bin:\$PATH"
+
 PATH="$BASHDIR:\$PATH"
 PATH="/c/.tools/mkvtoolnix:\$PATH"
 PATH="$BASHDIR/bin:\$PATH"
@@ -15,6 +22,8 @@ export PATH
 
 alias cdbash="cd '$BASHDIR'"
 alias cdbackend="cd '/c/project/backend'"
+alias cdfront="cd '/c/project/front'"
+alias cddocker="cd '/c/project/docker'"
 
 alias backup='backup.sh --drive=d itunes 유틸리티 develop project wallpaper editplus;\
               backup.sh --drive=y itunes 유틸리티 develop project'
@@ -24,6 +33,10 @@ EOF
 echo "generated '~/.bash_profile'"
 
 cat << EOF
+[ env ]
+  JAVA_HOME: $JAVA_HOME
+  MAVEN_HOME: $MAVEN_HOME
+
 [ git clone ]
 
   $ git clone https://github.com/lislroow/backend
@@ -32,6 +45,9 @@ cat << EOF
 
   alias cdbash="cd '$BASHDIR'"
   alias cdbackend="cd '/c/project/backend'"
+  alias cdbackend="cd '/c/project/backend'"
+  alias cdfront="cd '/c/project/front'"
+  alias cddocker="cd '/c/project/docker'"
   
   alias backup='backup.sh --drive=d itunes 유틸리티 develop project wallpaper editplus;\
                 backup.sh --drive=y itunes 유틸리티 develop project'
