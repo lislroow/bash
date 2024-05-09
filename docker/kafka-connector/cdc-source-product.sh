b2344cbf3d3595fa25214fe4e397125045cc30a4
김명구 <myeonggu.kim@kakao.com>
2024-05-09 15:20:00 +0900

    kafka cdc product 추가

---
+++ docker/kafka-connector/cdc-source-product.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+FUNCDIR=$( cd $( dirname $0 ) && pwd -P )
+eval "source \"$FUNCDIR/env.sh\""
+
+opts=$( getopt --options "" --longoptions "save,delete,status" -- $* )
+eval set -- $opts
+
+
+CONNECTOR="cdc-source-product"
+PORT="3405"
+DATABASE="market-product"
+TARGET_TABLE="product"
+CONNECTION_URL="jdbc:mariadb://local.mariadb-$DATABASE:$PORT/$DATABASE"
+INCREMENTING_COLUMN_NAME="id"
+
+while true; do
+  if [ -z $1 ]; then
+    break
+  fi
+  
+  case $1 in
+    --save)
+      curl --location --request PUT $CP_KAFKA_CONNECT'/connectors/'$CONNECTOR'/config' \
+      --header 'Content-Type: application/json' \
+      --data '{
+          "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
+          "connection.url": "'$CONNECTION_URL'",
+          "connection.user": "root",
+          "connection.password": "1",
+          "incrementing.column.name": "'$INCREMENTING_COLUMN_NAME'",
+          "timestamp.column.name": "modify_date",
+          "table.whitelist": "'$TARGET_TABLE'",
+          "schema.pattern": "'$TARGET_TABLE'",
+          "mode": "timestamp+incrementing",
+          "topic.prefix": "cdc_",
+          "tasks.max": "3"
+      }' | jq -C
+      ;;
+    --delete)
+      curl --location --request DELETE $CP_KAFKA_CONNECT'/connectors/'$CONNECTOR''
+      ;;
+    --status)
+      curl --location $CP_KAFKA_CONNECT'/connectors/'$CONNECTOR'?expand=info&expand=status' | jq -C
+      ;;
+    --)
+      ;;
+  esac
+  
+  shift
+done
+
--- docker/kafka-connector/cdc-status.sh
+++ docker/kafka-connector/cdc-status.sh
@@ -3,7 +3,7 @@
 FUNCDIR=$( cd $( dirname $0 ) && pwd -P )
 eval "source \"$FUNCDIR/env.sh\""
 
-opts=$( getopt --options "" --longoptions "customer" -- $* )
+opts=$( getopt --options "" --longoptions "customer,product" -- $* )
 eval set -- $opts
 
 while true; do
@@ -12,7 +12,7 @@ while true; do
   fi
   
   case $1 in
-    --customer | --delivery | --inventory | --order | --product)
+    --customer | --product)
       #curl --location --request GET $CP_KAFKA_CONNECT'/connectors?expand=info&expand=status' | jq -Cr 'to_entries[] | select(.key | startswith("cdc-") and endswith("'${1/--/}'")) | .value'
       curl --location --request GET $CP_KAFKA_CONNECT'/connectors?expand=status' | jq -Cr 'to_entries[] | select(.key | startswith("cdc-") and endswith("'${1/--/}'")) | .value'
       ;;
