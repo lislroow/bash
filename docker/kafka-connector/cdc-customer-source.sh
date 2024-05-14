f49e8851613f69434b1c74181e84c5fecf3415b5
김명구 <myeonggu.kim@kakao.com>
2024-05-14 21:05:51 +0900

    PROJECT_NAME 옵션 적용

--- docker/kafka-connector/cdc-customer-source.sh
+++ docker/kafka-connector/cdc-customer-source.sh
@@ -1,18 +1,51 @@
 #!/bin/bash
 
 FUNCDIR=$( cd $( dirname $0 ) && pwd -P )
+FILENAME=${0##*/} && FILENAME=${FILENAME%.*}
 eval "source \"$FUNCDIR/env.sh\""
 
-opts=$( getopt --options "" --longoptions "save,delete,status" -- $* )
+# usage
+function USAGE {
+  cat << EOF
+- USAGE
+Usage: ${0##*/} 
+          --prod-save
+          --local-save
+          --delete
+          --status
+
+EOF
+  exit 1
+}
+# //usage
+
+opts=$( getopt --options "h" --longoptions "prod-save,local-save,delete,status,help" -- $* )
 eval set -- $opts
 
+CONNECTOR="${FILENAME}"
 
-CONNECTOR="cdc-customer-source"
-PORT="3401"
-DATABASE="market-customer"
-TARGET_TABLE="customer"
-CONNECTION_URL="jdbc:mariadb://local.mariadb-$DATABASE:$PORT/$DATABASE"
-INCREMENTING_COLUMN_NAME="id"
+function save {
+  PORT="3401"
+  DATABASE="market-customer"
+  TARGET_TABLE="customer"
+  CONNECTION_URL="jdbc:mariadb://${PROJECT_NAME}.mariadb-$DATABASE:$PORT/$DATABASE"
+  INCREMENTING_COLUMN_NAME="id"
+  
+  curl --location --request PUT $CP_KAFKA_CONNECT'/connectors/'$CONNECTOR'/config' \
+  --header 'Content-Type: application/json' \
+  --data '{
+      "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
+      "connection.url": "'$CONNECTION_URL'",
+      "connection.user": "root",
+      "connection.password": "1",
+      "timestamp.column.name": "modify_date",
+      "table.whitelist": "'$TARGET_TABLE'",
+      "schema.pattern": "'$TARGET_TABLE'",
+      "mode": "timestamp",
+      "topic.prefix": "cdc_",
+      "tasks.max": "3"
+  }' | jq -C
+}
 
 while true; do
   if [ -z $1 ]; then
@@ -20,32 +53,24 @@ while true; do
   fi
   
   case $1 in
-    --save)
-      curl --location --request PUT $CP_KAFKA_CONNECT'/connectors/'$CONNECTOR'/config' \
-      --header 'Content-Type: application/json' \
-      --data '{
-          "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
-          "connection.url": "'$CONNECTION_URL'",
-          "connection.user": "root",
-          "connection.password": "1",
-          "timestamp.column.name": "modify_date",
-          "table.whitelist": "'$TARGET_TABLE'",
-          "schema.pattern": "'$TARGET_TABLE'",
-          "mode": "timestamp",
-          "topic.prefix": "cdc_",
-          "tasks.max": "3"
-      }' | jq -C
+    --prod-save)
+      PROJECT_NAME="prod"
+      save
+      ;;
+    --local-save)
+      PROJECT_NAME="local"
+      save
       ;;
     --delete)
       curl --location --request DELETE $CP_KAFKA_CONNECT'/connectors/'$CONNECTOR''
       ;;
     --status)
-      curl --location $CP_KAFKA_CONNECT'/connectors/'$CONNECTOR'?expand=info&expand=status' | jq -C
+      curl --location --request GET $CP_KAFKA_CONNECT'/connectors/'$CONNECTOR'?expand=info&expand=status' | jq -C
       ;;
-    --)
+    --help | -h)
+      USAGE
       ;;
   esac
   
   shift
 done
-
--- docker/kafka-connector/cdc-product-sink-inventory.sh
+++ docker/kafka-connector/cdc-product-sink-inventory.sh
@@ -1,18 +1,57 @@
 #!/bin/bash
 
 FUNCDIR=$( cd $( dirname $0 ) && pwd -P )
+FILENAME=${0##*/} && FILENAME=${FILENAME%.*}
 eval "source \"$FUNCDIR/env.sh\""
 
-opts=$( getopt --options "" --longoptions "save,delete,status" -- $* )
+# usage
+function USAGE {
+  cat << EOF
+- USAGE
+Usage: ${0##*/} 
+          --prod-save
+          --local-save
+          --delete
+          --status
+
+EOF
+  exit 1
+}
+# //usage
+
+opts=$( getopt --options "h" --longoptions "prod-save,local-save,delete,status,help" -- $* )
 eval set -- $opts
 
+CONNECTOR="${FILENAME}"
 
-CONNECTOR="cdc-product-sink-inventory"
-PORT="3403"
-DATABASE="market-inventory"
-TARGET_TABLE="product"
-CONNECTION_URL="jdbc:mariadb://local.mariadb-$DATABASE:$PORT/$DATABASE"
-PK_FIELDS="id"
+function save {
+  PORT="3403"
+  DATABASE="market-inventory"
+  TARGET_TABLE="product"
+  CONNECTION_URL="jdbc:mariadb://${PROJECT_NAME}.mariadb-$DATABASE:$PORT/$DATABASE"
+  PK_FIELDS="id"
+  curl --location --request PUT $CP_KAFKA_CONNECT'/connectors/'$CONNECTOR'/config' \
+  --header 'Content-Type: application/json' \
+  --data '{
+      "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+      "topics": "cdc_'$TARGET_TABLE'",
+      "connection.url": "'$CONNECTION_URL'",
+      "connection.user": "root",
+      "connection.password": "1",
+      "table.name.format": "'$DATABASE'.'$TARGET_TABLE'",
+      "auto.create": "true",
+      "auto.evolve": "true",
+      "insert.mode": "upsert",
+      "pk.fields": "'$PK_FIELDS'",
+      "pk.mode": "record_value",
+      "delete.enabled": "false",
+      "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+      "key.converter.schemas.enable": "true",
+      "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+      "value.converter.schemas.enable": "true",
+      "tasks.max": "1"
+  }' | jq -C
+}
 
 while true; do
   if [ -z $1 ]; then
@@ -20,28 +59,13 @@ while true; do
   fi
   
   case $1 in
-    --save)
-      curl --location --request PUT $CP_KAFKA_CONNECT'/connectors/'$CONNECTOR'/config' \
-      --header 'Content-Type: application/json' \
-      --data '{
-          "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
-          "topics": "cdc_'$TARGET_TABLE'",
-          "connection.url": "'$CONNECTION_URL'",
-          "connection.user": "root",
-          "connection.password": "1",
-          "table.name.format": "'$DATABASE'.'$TARGET_TABLE'",
-          "auto.create": "true",
-          "auto.evolve": "true",
-          "insert.mode": "upsert",
-          "pk.fields": "'$PK_FIELDS'",
-          "pk.mode": "record_value",
-          "delete.enabled": "false",
-          "key.converter": "org.apache.kafka.connect.json.JsonConverter",
-          "key.converter.schemas.enable": "true",
-          "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-          "value.converter.schemas.enable": "true",
-          "tasks.max": "1"
-      }' | jq -C
+    --prod-save)
+      PROJECT_NAME="prod"
+      save
+      ;;
+    --local-save)
+      PROJECT_NAME="local"
+      save
       ;;
     --delete)
       curl --location --request DELETE $CP_KAFKA_CONNECT'/connectors/'$CONNECTOR''
@@ -49,10 +73,10 @@ while true; do
     --status)
       curl --location --request GET $CP_KAFKA_CONNECT'/connectors/'$CONNECTOR'?expand=info&expand=status' | jq -C
       ;;
-    --)
+    --help | -h)
+      USAGE
       ;;
   esac
   
   shift
 done
-
