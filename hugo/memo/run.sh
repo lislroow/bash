f7946739e6cc2619659be624a2f041cef32984a8
김명구 <myeonggu.kim@kakao.com>
2024-09-23 03:16:26 +0900

    코드 정리: idea 스크립트 problems 반영

--- hugo/memo/run.sh
+++ hugo/memo/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE_DIR=$( cd $( dirname $0 ) && pwd -P )
+BASE_DIR=$( cd "$( dirname "$0" )" && pwd -P )
 HUGO=${BASE_DIR%/*}/bin/hugo
 
 HUGO_VERSION=$( $HUGO version )
@@ -14,7 +14,7 @@ function server {
   
   HUGO_CMD="${HUGO} server ${HUGO_OPTS}"
   echo -e "\e[0;32m${HUGO_CMD}\e[0m"
-  eval ${HUGO_CMD}
+  eval "${HUGO_CMD}"
 }
 
 function deploy {
@@ -23,19 +23,19 @@ function deploy {
   
   HUGO_CMD="${HUGO} ${HUGO_OPTS}"
   echo -e "\e[0;32m${HUGO_CMD}\e[0m"
-  eval ${HUGO_CMD}
+  eval "${HUGO_CMD}"
   
   CMD="rm -rf lislroow.github.io/*"
   echo -e "\e[0;32m${CMD}\e[0m"
-  eval ${CMD}
+  eval "${CMD}"
   
   CMD="cp -R public/* lislroow.github.io/"
   echo -e "\e[0;32m${CMD}\e[0m"
-  eval ${CMD}
+  eval "${CMD}"
   
   CMD="cd lislroow.github.io/ && git add . && git commit -m 'update' && git push"
   echo -e "\e[0;32m${CMD}\e[0m"
-  eval ${CMD}
+  eval "${CMD}"
 }
 
 function staging {
@@ -44,11 +44,11 @@ function staging {
   
   HUGO_CMD="${HUGO} ${HUGO_OPTS}"
   echo -e "\e[0;32m${HUGO_CMD}\e[0m"
-  eval ${HUGO_CMD}
+  eval "${HUGO_CMD}"
   
   CMD="tar cfz - public/* | ssh root@172.28.200.101 'tar xfz - --strip-components=1 -C /sorc/memo'"
   echo -e "\e[0;32m${CMD}\e[0m"
-  eval ${CMD}
+  eval "${CMD}"
 }
 
 case $1 in
