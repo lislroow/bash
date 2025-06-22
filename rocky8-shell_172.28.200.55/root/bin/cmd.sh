32ea5eeff4af34cac4665de25b9189e9e4a550fc
김명구 <myeonggu.kim@kakao.com>
2025-06-22 23:49:47 +0900

    feature: `cmd.sh` ksh93

--- rocky8-shell_172.28.200.55/root/bin/cmd.sh
+++ rocky8-shell_172.28.200.55/root/bin/cmd.sh
@@ -5,6 +5,7 @@ OUTLOG=/app/jeus/e1002317/cmd_$(date +%Y%m%d).log
 UNIT_GB=000000000
 UNIT_MB=000000
 UNIT_KB=000
+UNIT_B=
 
 # common
 function USAGE {
@@ -19,18 +20,32 @@ EOF
 }
 
 function LOG {
-  local str="$@"
+  typeset level="$1"
+  shift
+  typeset str="$@"
   TS=$(date +%Y-%m-%d\ %H:%M:%S)
-  if [ ${DEBUG_MODE} -eq 1 ]; then
-    echo "[${TS}][debug]" "${str}" | tee -a $OUTLOG
-  else
-    echo "[${TS}][info]" "${str}" | tee -a $OUTLOG
+  if [ $level -gt $LOG_LEVEL ]; then
+      return
   fi
+  case "$level" in
+    0)
+      echo "[${TS}][error]" "${str}" | tee -a $OUTLOG
+      ;;
+    1)
+      echo "[${TS}][info]" "${str}" | tee -a $OUTLOG
+      ;;
+    2)
+      echo "[${TS}][debug]" "${str}" | tee -a $OUTLOG
+      ;;
+    3)
+      echo "[${TS}][trace]" "${str}" | tee -a $OUTLOG
+      ;;
+  esac
 }
 
 function RUN {
-  local str="$@"
-  LOG "${str}"
+  typeset str="$@"
+  LOG 2 "${str}"
   /bin/ksh -c "${str}"
 }
 
@@ -38,33 +53,37 @@ function RUN {
 
 # execute
 function execute {
-  LOG "$@"
-  IFS=','; set -a tasks $@; unset IFS
-  local cmd
-  for task in "${tasks[@]}"; do
+  LOG 3 "$@"
+  typeset cmd
+  for task in "$@"; do
     case $task in
     clear-log)
-      cmd="find /logs/pgm -type f -size +200${UNIT_KB} -mmin +60"
-      LOG=$cmd
+      cmd="find /root/bin -type f -size +1${UNIT_B}"
+      LOG 2 $cmd
       set -A files $(/bin/ksh -c "${cmd}")
       typeset -a entries
       for file in "${files[@]}"; do
         if [[ "$file" == *" "* ]]; then
-          LOG "ERROR: filename contains spaces. $file"
+          LOG 0 "ERROR: filename contains spaces. $file"
           continue
         fi
-        entries[${entries[@]}]=$file
-        LOG $(ls -al ${file})
+        entries+=($file)
+        LOG 1 $(ls -al ${file})
       done
-      for file in "${entries[@]}"; do
-        RUN "cat /dev/null > $file"
-      done
+      #for file in "${entries[@]}"; do
+      #  RUN "cat /dev/null > $file"
+      #done
       ;;
     kill-batch)
-      cmd="ps -ef | grep -v grep | grep btuser | grep JobRunnerMain | awk '{
+      #cmd="ps -ef | grep -v grep | grep btuser | grep JobRunnerMain | awk '{
+      cmd="ps -ef | grep -v grep | grep -v awk | awk '{
         for (i=1; i<=NF; i++) {
-          if ($i ~ /JobRunnerMain$/) {
-            print $2 " " $(i+1)
+          if (\$i ~ /\/usr/) {
+            printf \"[%d] \", \$2
+            for (j=i; j<=NF; j++) {
+              printf \"%s \", \$j
+            }
+            printf \"\n\"
             break
           }
         }
@@ -76,10 +95,10 @@ function execute {
   done
 }
 
-OPTIONS="h,d"
-LONGOPTIONS="help,execute=.*"
 DEBUG_MODE=0
-function main {
+OPTIONS="h,d"
+LONGOPTIONS="help,execute=*"
+function main_gnu {
   opts=$( getopt --options "${OPTIONS}" \
                  --longoptions "${LONGOPTIONS}" \
                  -- "$@" )
@@ -89,6 +108,7 @@ function main {
     if [ -z "$1" ]; then
       break
     fi
+    echo "$1"
     case $1 in
     -h)
       USAGE
@@ -96,8 +116,9 @@ function main {
     -d)
       DEBUG_MODE=1
       ;;
-    --execute=*)
-      local list="${1#--execute=}"
+    --exec=*)
+      echo "hello"
+      local list="${1#--exec=}"
       if [ ${#list[@]} -gt 0 ]; then
         execute "${list[@]}"
       fi
@@ -108,9 +129,36 @@ function main {
         status "${list[@]}"
       fi
       ;;
-    *)
+    --)
       ;;
+    esac
+    shift
   done
 }
 
+LOG_LEVEL=0
+function main {
+  typeset -a tasks=()
+  while getopts "he:v" opt; do
+    case "$opt" in
+      h|\?)
+        USAGE
+        ;;
+      v)
+        LOG_LEVEL=$((LOG_LEVEL+1))
+        echo "log level: ${LOG_LEVEL}"
+        ;;
+      e)
+        tasks+=($OPTARG)
+        ;;
+    esac
+  done
+  echo "tasks: ${tasks[@]}"
+  if [ ${#tasks[@]} -ge 0 ]; then
+    execute "${tasks[@]}"
+  fi
+  shift $((OPTIND - 1))
+}
+
 main "$@"
+
