8d9904c923c5246d5585a87b62425601d5ff261b
김명구 <myeonggu.kim@kakao.com>
2025-06-29 10:05:11 +0900

    feature: `clear-log.sh`, `common.inc` ksh88

--- rocky8-shell_172.28.200.55/root/bin/clear-log.sh
+++ rocky8-shell_172.28.200.55/root/bin/clear-log.sh
@@ -1,10 +1,6 @@
 #!/bin/ksh
-
-# env
-UNIT_GB=000000000
-UNIT_MB=000000
-UNIT_KB=000
-UNIT_B=
+BASEDIR=$( cd "$( dirname "$0" )" && pwd -P )
+SYSNAME=$(uname -a | awk '{ print $1 }')
 
 # common
 function USAGE {
@@ -15,11 +11,104 @@ Usage:
      s는 find 명령어에 전달되는 값입니다.
 -m:  분(min) 지난 파일을 선택합니다.
      m 은 find 명령어에 전달되는 값입니다.
--a:  선택된 파일을 a 파일명으로 압축(archive) 합니다.
-     tar cvfz 명령어가 실행됩니다.
--e   선택된 파일을 제거(erase) 합니다.
+-d:  일(day) 지난 파일을 선택합니다.
+     d 는 find 명령어에 전달되는 값입니다.
+-a:  선택된 파일을 .gz 으로 압축(archive) 합니다.
+-t   선택된 파일을 truncate 합니다.
+     선택된 파일을 0byte 로 truncate 합니다.
 ───────────────────────────────────────────────────────────────
 EOF
   exit 1
 }
+OPTIONS="s:m:d:at"
+source ${BASEDIR}/common.inc
+# -- common
 
+
+# init
+typeset SIZE
+typeset MMIN
+typeset MTIME
+typeset ARCHIVE_YN=0
+typeset TRUNCATE_YN=0
+
+function init {
+  while getopts "${_OPTIONS},${OPTIONS}" opt; do
+    case "$opt" in
+      s)
+        SIZE=${OPTARG}
+        ;;
+      m)
+        MMIN=${OPTARG}
+        ;;
+      d)
+        MTIME=${OPTARG}
+        ;;
+      a)
+        ARCHIVE_YN=1
+        ;;
+      t)
+        TRUNCATE_YN=1
+        ;;
+    esac
+  done
+  shift $((OPTIND - 1))
+}
+
+init "$@"
+# -- init
+
+LOG 1 "== script started"
+START_TIME=$(date +%s)
+
+## find log files
+function FindLogFiles {
+  typeset FIND_COND="-type f ! -name '*.gz'"
+  if [ -n "${SIZE}" ]; then
+    FIND_COND="${FIND_COND} -size ${SIZE}"
+  fi
+  if [ -n "${MMIN}" ]; then
+    FIND_COND="${FIND_COND} -mmin ${MMIN}"
+  fi
+  if [ -n "${MMIN}" ] && [ -n "${MTIME}" ]; then
+    FIND_COND="${FIND_COND} -o"
+  fi
+  if [ -n "${MTIME}" ]; then
+    FIND_COND="${FIND_COND} -mtime ${MTIME}"
+  fi
+  typeset str="find /logs ${FIND_COND}"
+  LOG 1 $str
+  typeset -a list=(`$str`)
+  echo ${list[@]}
+}
+
+function ProcessFiles {
+  list="$@"
+  for item in ${list[@]}; do
+    typeset str
+    if [[ "${item}" =~ \.gz$ ]]; then
+      LOG 1 "(exclude) gz file, ${item}"
+    fi
+    if [ -e "${item}.gz" ]; then
+      LOG 1 "(exclude) already exist, ${item}.gz"
+    else
+      str="gzip -c ${item} > ${item}.gz"
+      RUN $str
+    fi
+    str="tail -n 4000 ${item} > temp.log && cat /dev/null > ${item} && cat temp.log > ${item}"
+    LOG 1 $str
+    #RUN $str
+  done
+}
+
+list=(`FindLogFiles`)
+PRINT_LIST ${list[@]}
+
+if [ ${ARCHIVE_YN} == 1 ] || [ ${TRUNCATE_YN} == 1 ]; then
+  ProcessFiles "${list[@]}"
+fi
+
+
+END_TIME=$(date +%s)
+ELPASED_TIME=$((END_TIME - START_TIME))
+LOG 1 "== script completed (elapsed time: ${ELPASED_TIME})"
